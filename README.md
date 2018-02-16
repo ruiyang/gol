bb4b87643ccded2dc40771efc6b86ce6c8e91a8c
Rui Yang <ryang@bglcorp.com.au>
2018-02-16 16:23:52 +1100

    game of life initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gol
+
+Clojure implementation of [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life)
---
+++ src/gol/core.clj
@@ -0,0 +1,50 @@
+(ns gol.core)
+
+(defn get-all-cells-to-check [cells]
+  (->> (map (fn [[x y]]
+             (for [step_x [-1 0 1]
+                   steps_y [-1 0 1]]
+               [(+ x step_x) (+ y steps_y)]))
+           cells)
+      (apply concat)
+      distinct))
+
+(defn- get-neighbours [[x y]]
+  (filter #(not (nil? %))
+          (for [step_x [-1 0 1]
+                             step_y [-1 0 1]]
+                  (if (= step_x step_y 0)
+                    nil
+                    [(+ x step_x) (+ y step_y)]))))
+
+(defn get-live-neighbour-size [[x y] state-matrix]
+  (let [neighbours (set (get-neighbours [x y]))
+        state-matrix (set state-matrix)
+        live-neighbours (clojure.set/intersection neighbours state-matrix)]
+    (count live-neighbours)))
+
+
+(defn calculate-cell-next-state [live? live-neighbour-size]
+  (if live?
+    (cond
+      (< live-neighbour-size 2) :die
+      (= live-neighbour-size 2) :live
+      (= live-neighbour-size 3) :live
+      (> live-neighbour-size 3) :die)
+    (cond
+      (= live-neighbour-size 3) :live
+      :else :die)))
+
+(defn is-cell-alive? [cell matrix]
+  (not= (some #(= cell %) matrix)
+        nil))
+
+(defn gol-next-state [matrix]
+  (let [cells-next-state (map (fn [cell]
+                                (let [live? (is-cell-alive? cell matrix)
+                                      next-state (calculate-cell-next-state live? (get-live-neighbour-size cell matrix))]
+                                  (if (= next-state :live)
+                                    cell
+                                    nil)))
+                              (get-all-cells-to-check matrix))]
+    (filter #(not= nil %) cells-next-state)))
